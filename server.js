b44754837ec6280f83adce6ef350314ca4312d51
Rachel Mills <rachel.h.mills@gmail.com>
2017-06-30 15:59:49 +0100

    Add POST for todos.

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Go for run',
-	completed: false
-},
-{
-	id: 2,
-	description: 'Talk to Ed',
-	completed: false
-},
-{
-	id: 3,
-	description: 'Français Authentique module',
-	complete: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo-api root');
@@ -45,6 +35,13 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = todoNextId++
+	todos.push(body);
+	res.json(body);
 });
 
 app.listen(PORT, function() {
