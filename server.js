2ceca746e8804c8fe4b8927682bde65391ee8598
Rachel Mills <rachel.h.mills@gmail.com>
2017-07-01 12:15:13 +0100

    Add put totos route.

--- server.js
+++ server.js
@@ -52,7 +52,35 @@ app.delete('/todos/:id', function(req, res) {
 	
 		res.status(404).json({"error" : "no todo item found with that id"});
 	}
-})
+});
+
+app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodoItem = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodoItem) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} else {
+		// never provided attribute, no problem here
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description.trim();
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodoItem, validAttributes);
+	res.json(matchedTodoItem);
+});
 
 app.listen(PORT, function() {
 	console.log('Express listening on port: ' + PORT);
